a1817379ad9e57b2abf5bd2ff5fc075678023221
Katie Russ <katie.k7r@gmail.com>
2016-07-02 21:27:32 -0400

    Initial commit [Yeoman]

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appletsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appletsApp
+ */
+angular.module('appletsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appletsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appletsApp
+ */
+angular.module('appletsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
